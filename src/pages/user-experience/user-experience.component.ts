8d1debacd428d37a2775658a020660c4aca66558
Jpolloj <jjcm_03@hotmail.com>
2024-05-10 22:33:16 -0500

    Crear proyecto, componentes header y footer, page principal inicio

---
+++ src/pages/user-experience/user-experience.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-experience',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-experience.component.html',
+  styleUrl: './user-experience.component.scss'
+})
+export class UserExperienceComponent {
+
+}
