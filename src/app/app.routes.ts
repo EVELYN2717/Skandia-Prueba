4303183be28e8f75a7c824b9941508522f40858b
Jpolloj <jjcm_03@hotmail.com>
2024-05-11 18:53:57 -0500

    feat: implementar componente footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserExperienceComponent } from '../pages/user-experience/user-experience.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/user-experience.component', pathMatch: 'full'},
+    {path: 'user-experience.component', component: UserExperienceComponent}
+];
--- src/pages/user-experience/user-experience.component.ts
+++ src/pages/user-experience/user-experience.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-user-experience',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './user-experience.component.html',
   styleUrl: './user-experience.component.scss'
 })
