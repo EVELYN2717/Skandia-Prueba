1010861f475b5359ef02e04a98363d15846970bd
Jpolloj <jjcm_03@hotmail.com>
2024-05-12 03:56:22 -0500

    fix: ajustar estilos pendientes, crear boton asociar objetivo

--- src/pages/user-experience/user-experience.component.ts
+++ src/pages/user-experience/user-experience.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { CardGoalsComponent } from '../../components/card-goals/card-goals.component';
 
 @Component({
   selector: 'app-user-experience',
   standalone: true,
   imports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CardGoalsComponent
   ],
   templateUrl: './user-experience.component.html',
   styleUrl: './user-experience.component.scss'
