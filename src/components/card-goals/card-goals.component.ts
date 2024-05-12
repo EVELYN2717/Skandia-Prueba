d737da9d05b75c67d77ca3bcab0f77d4d4b885da
Jpolloj <jjcm_03@hotmail.com>
2024-05-12 03:49:50 -0500

    fix: ajustar estilos pendientes, crear boton asociar objetivo

---
+++ src/components/card-goals/card-goals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-goals',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-goals.component.html',
+  styleUrl: './card-goals.component.scss'
+})
+export class CardGoalsComponent {
+
+}
