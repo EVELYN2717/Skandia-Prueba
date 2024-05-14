6a396ae4713ddb16e794f67c6f8fa497227b4f5d
Jpolloj <jjcm_03@hotmail.com>
2024-05-14 03:04:24 -0500

    feat: ajustes estilos cards products balance

--- src/components/cards-products-balance/cards-products-balance.component.ts
+++ src/components/cards-products-balance/cards-products-balance.component.ts
@@ -1,15 +1,19 @@
 import { SlicePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, inject } from '@angular/core';
+import { CardsProductsBalanceService } from '../../services/cards-products-balance.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-cards-products-balance',
   standalone: true,
-  imports: [SlicePipe],
+  imports: [SlicePipe, HttpClientModule],
+  providers: [CardsProductsBalanceService],
   templateUrl: './cards-products-balance.component.html',
-  styleUrl: './cards-products-balance.component.scss'
+  styleUrl: './cards-products-balance.component.scss',
 })
-export class CardsProductsBalanceComponent implements OnInit {
+export class CardsProductsBalanceComponent implements OnInit,  AfterViewInit {
 
+  
   public iconCard =[
     'Icon awesome-medal.svg',
     'Trofeo.svg',
@@ -21,9 +25,53 @@ export class CardsProductsBalanceComponent implements OnInit {
     '#A5D7E1',
     '#63AABC',
     '#FF9460'
-  ]
+  ];
+
+  public listCard: any;
+  public direction: string = '';
+  public statusLeftButton = 'unused';
+  public statusRightButton = 'used';
+  
+  constructor(public cardsProductService: CardsProductsBalanceService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  leftCards() {
+    document.getElementById("0-card")!.style.display = "block";
+    document.getElementById("1-card")!.style.display = "block";
+    document.getElementById("2-card")!.style.display = "block";
+    document.getElementById("3-card")!.style.display = "none";
+    document.getElementById("4-card")!.style.display = "none";
+
+    this.statusLeftButton = 'unused';
+    this.statusRightButton = 'used';
+  }
+
+  rightCards() {
+    document.getElementById("0-card")!.style.display = "none";
+    document.getElementById("1-card")!.style.display = "none";
+    document.getElementById("2-card")!.style.display = "block";
+    document.getElementById("3-card")!.style.display = "block";
+    document.getElementById("4-card")!.style.display = "block";
+    this.statusLeftButton = 'used';
+    this.statusRightButton = 'unused';
+  }
+
+  ngAfterViewInit() {
+    this.getProducts();
+  }
 
 
-  ngOnInit(): void {}
+  async getProducts() {
+    await this.cardsProductService.getCardProducts().then( (response) => {
+      this.listCard = response;
+      document.getElementById("3-card")!.style.display = "none";
+      document.getElementById("4-card")!.style.display = "none";
+    });
+    
+  }
+  
 
 }
--- src/pages/user-experience/user-experience.component.ts
+++ src/pages/user-experience/user-experience.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { CardGoalsComponent } from '../../components/card-goals/card-goals.component';
 import { NavigationMenuComponent } from '../../components/navigation-menu/navigation-menu.component';
+import { CardsProductsBalanceComponent } from '../../components/cards-products-balance/cards-products-balance.component';
 
 @Component({
   selector: 'app-user-experience',
@@ -11,7 +12,8 @@ import { NavigationMenuComponent } from '../../components/navigation-menu/naviga
     HeaderComponent,
     FooterComponent,
     CardGoalsComponent,
-    NavigationMenuComponent
+    NavigationMenuComponent,
+    CardsProductsBalanceComponent
   ],
   templateUrl: './user-experience.component.html',
   styleUrl: './user-experience.component.scss'
